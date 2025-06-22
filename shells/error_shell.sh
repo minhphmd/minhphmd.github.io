85f8826bbae1feeed681bec3c0000519440b4087
Minh Pham <phm.d.minh@gmail.com>
2025-06-22 23:29:58 +0700

    Remove bad shells

--- shells/error_shell.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ls -la
-
-if [[ $? -eq 0 ]] 
-	echo "Everything went as well as expected"
-fi
--- shells/warning_shell.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ls -la
-
-if [[ $? -neq 0 ]]; then
-	echo "Something went wrong!"
-fi
